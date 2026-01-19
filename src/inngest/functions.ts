77ee106cc8e10fce1427aefbc09820774919c797
newbie-del <mikehanderson108@proton.me>
2026-01-19 21:25:05 +0530

    07:ai providers

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,25 +1,21 @@
 import prisma from "@/lib/db";
 import { inngest } from "./client";
+import { createGoogleGenerativeAI, google } from "@ai-sdk/google";
+import { generateText } from "ai";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world", },
-  { event: "test/hello.world" },
+const googleAI = createGoogleGenerativeAI();
+
+export const execute = inngest.createFunction(
+  { id: "execute-ai", },
+  { event: "execute/ai" },
   async ({ event, step }) => {
-    //Fetching the video
-    await step.sleep("fetching", "5s");
-
-    //Transcribing
-    await step.sleep("transcribing", "5s");
-
-    //Sending transcription to AI
-    await step.sleep("sending-to-AI", "5s");
-
-    await step.run("create-workflow", () => {
-      return prisma.workflow.create({
-        data: {
-          name: "workflow-from-inngest",
-        },
-      });
+    await step.sleep("pretend", "5s");
+    const { steps } = await step.ai.wrap("gemini-generated-text", generateText, 
+      {
+      model: google("gemini-2.5-flash"),
+      system: "You are a helpful assistant.",
+      prompt: "What is 2+2?"
     });
+    return steps;
   },
 );
