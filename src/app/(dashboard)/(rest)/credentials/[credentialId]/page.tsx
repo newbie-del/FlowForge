f4a9d88982dc21db892c57d02cc9e222be3a5e93
newbie-del <mikehanderson108@proton.me>
2026-01-20 21:21:55 +0530

    09: sidebar layout

---
+++ src/app/(dashboard)/(rest)/credentials/[credentialId]/page.tsx
@@ -0,0 +1,18 @@
+import { requireAuth } from "@/lib/auth-utils";
+
+interface PageProps {
+    params: Promise<{
+        credentialId: string;   
+    }>
+};
+
+
+
+const Page = async ({ params }: PageProps) => {
+    await requireAuth();
+    const { credentialId } = await params;
+
+    return <p> Credentials id: {credentialId} </p>
+};
+
+export default Page;
